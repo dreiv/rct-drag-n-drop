124cd956462070d459d0bb4f89f686f2834060b6
dreiv <dreivoicu@gmail.com>
2018-07-17 23:04:49 +0300

    drag and drop implementation works :D

--- src/App.js
+++ src/App.js
@@ -3,11 +3,56 @@ import { fillRange } from './utils'
 import './App.css'
 
 class App extends Component {
-	items = fillRange(1, 10)
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			items: fillRange(1, 10),
+			dragging: false,
+			dropKey: 0,
+		}
+	}
+
+	drag = () => {
+		this.setState({ dragging: true })
+	}
+
+	drop = key => {
+		this.setState({ dragging: false })
+		this.switchItemByKeys(key, this.state.dropKey)
+	}
+
+	dragOver = dropKey => {
+		this.setState(prevState => {
+			if (prevState.dropKey !== dropKey) {
+				return { dropKey }
+			}
+		})
+	}
+
+	switchItemByKeys = (key, dropKey) => {
+		const { items } = this.state
+		const aux = items[key]
+
+		items[key] = items[dropKey]
+		items[dropKey] = aux
+
+		this.setState({ items })
+	}
 
 	render() {
-		const renderedItems = this.items.map((item, idx) => (
-			<li key={idx}>{item}</li>
+		const { items } = this.state
+
+		const renderedItems = items.map((item, idx) => (
+			<li
+				key={idx}
+				onDragStart={this.drag}
+				onDragOver={() => this.dragOver(idx)}
+				onDragEnd={() => this.drop(idx)}
+				draggable="true"
+			>
+				{item}
+			</li>
 		))
 
 		return (
